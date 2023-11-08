9d5255764bb04411dac7d9142786bbb7c20aeafa
itsmdhammad <itsmdhammad@gmail.com>
2023-11-08 09:00:14 +0500

    message

--- Test.js
+++ Test.js
@@ -6,3 +6,11 @@ const f1=()=> {
     console.log("Changing this fucntion for conflict");
 }
 
+const f2=()=> {
+    console.log("My F2 Function");
+}
+
+const f3=()=> {
+    console.log("F3 Function");
+}
+
